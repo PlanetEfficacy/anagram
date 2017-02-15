d2a198c4d2fc5cf1ed86e74ca59b91923f901461
PlanetEfficacy <jspevack@gmail.com>
2017-02-14 21:27:46 -0700

    Add scope for proper nouns

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,9 @@
 class Word < ApplicationRecord
   validates_presence_of :value
   validates_uniqueness_of :value
+
+  def self.exclude_proper_nouns
+    where("ASCII(left(value, 1)) between ASCII('a') and ASCII('z')")
+
+  end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Word, type: :model do
     it { should validate_presence_of(:value) }
     it { should validate_uniqueness_of(:value) }
   end
+
+  context "scope" do
+    it "can be scoped to exclude proper nounds" do
+      create :word, value: 'pants'
+      create :word, value: 'Pants'
+
+      result = Word.exclude_proper_nouns
+
+      expect(result.count).to eq(1)
+      expect(result.first.value).to eq('pants')
+    end
+  end
 end
